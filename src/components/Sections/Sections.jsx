e3067bad1dbcee5fc5541c3162297fa400d664d7
alex <al3xback@gmail.com>
2024-06-11 21:09:45 +0700

    changed className to class

--- src/components/Sections/Sections.jsx
+++ src/components/Sections/Sections.jsx
@@ -2,41 +2,41 @@ import styles from './Sections.module.css';
 
 export default function Sections() {
 	return (
-		<div className={styles.sections}>
+		<div class={styles.sections}>
 			<section
-				className={`${styles.section} ${styles['section-join-community']}`}
+				class={`${styles.section} ${styles['section-join-community']}`}
 			>
-				<h2 className={styles.title}>Join our community</h2>
-				<p className={styles.subtitle}>
+				<h2 class={styles.title}>Join our community</h2>
+				<p class={styles.subtitle}>
 					30-day, hassle-free money back guarantee
 				</p>
-				<p className={styles.desc}>
+				<p class={styles.desc}>
 					Gain access to our full library of tutorials along with
 					expert code reviews. Perfect for any developers who are
 					serious about honing their skills.
 				</p>
 			</section>
-			<div className={styles.group}>
+			<div class={styles.group}>
 				<section
-					className={`${styles.section} ${styles['section-monthly-subsription']}`}
+					class={`${styles.section} ${styles['section-monthly-subsription']}`}
 				>
-					<h3 className={styles.title}>Monthly Subscription</h3>
-					<p className={styles.price}>
-						<span className={styles.num}>$29</span>
-						<span className={styles.label}>per month</span>
+					<h3 class={styles.title}>Monthly Subscription</h3>
+					<p class={styles.price}>
+						<span class={styles.num}>$29</span>
+						<span class={styles.label}>per month</span>
 					</p>
-					<p className={styles.desc}>
+					<p class={styles.desc}>
 						Full access for less than $1 a day
 					</p>
-					<a href='#' className='btn btn--primary btn--full'>
+					<a href='#' class='btn btn--primary btn--full'>
 						Sign Up
 					</a>
 				</section>
 				<section
-					className={`${styles.section} ${styles['section-why-us']}`}
+					class={`${styles.section} ${styles['section-why-us']}`}
 				>
-					<h3 className={styles.title}>Why Us</h3>
-					<ul className={styles.list}>
+					<h3 class={styles.title}>Why Us</h3>
+					<ul class={styles.list}>
 						<li>Tutorials by industry experts</li>
 						<li>Peer &amp; expert code review</li>
 						<li>Coding exercises</li>
